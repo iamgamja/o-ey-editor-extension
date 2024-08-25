4fb4fe9b285bff427675ce5affcd1db88f98a243
김주원 <hmmthinking@naver.com>
2024-08-25 10:40:27 +0000

    feat: editor

--- entrypoints/content.ts
+++
@@ -1,6 +0,0 @@
-export default defineContentScript({
-  matches: ['*://acmicpc.net/problem/*'],
-  main() {
-    console.log(browser)
-  },
-})
---
+++ utils/insertForm.ts
@@ -0,0 +1,30 @@
+function isTextarea(ele: Element) {
+  if (['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(ele.tagName.toLowerCase()))
+    return false
+  if (ele.id === 'problem_title')
+    return false
+  return true
+}
+
+export default function insertForm({htmls, idx, value, ele}: {htmls: string[], idx: number, value: string, ele: Element}) {
+  const form = isTextarea(ele)  ? document.createElement('textarea') : document.createElement('input')
+  
+  form.classList.add('oey-form', 'form-control')
+  
+  form.value = value
+  form.addEventListener('change', () => {
+    htmls[idx] = form.value
+  })
+
+  ele.after(form)
+
+  if (isTextarea(ele)) {
+    function autoHeight() {
+      form.style.height = '5px'
+      form.style.height = `${form.scrollHeight + 10}px`
+    }
+  
+    autoHeight()
+    form.addEventListener('keyup', autoHeight)
+  }
+}
